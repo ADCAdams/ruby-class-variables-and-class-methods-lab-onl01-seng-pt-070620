42ca98fbd70d4ae6a8a439947221c818e07dbb4a
Andy Adams <andrewdcadams@gmail.com>
2020-07-22 21:39:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,13 @@ class Song
   SONGCOUNT = 0 
   GENRECOUNT = 0 
   
+  attr_accessor :name, :artist, :genre 
   
+  def initialize(newName, newArtist, newGenre)
+    @name = newName
+    @artist = newArtist
+    @genre = newGenre
+  end
   
   
   
